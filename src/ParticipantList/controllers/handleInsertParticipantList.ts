698afc5714b54dbdd2be353bda96ef5476deb52d
Rafael Garcia <rafaelgmotion@gmail.com>
2026-01-02 23:41:47 -0300

    Atualizar o handleInsertParticipantList.ts

--- src/ParticipantList/controllers/handleInsertParticipantList.ts
+++ src/ParticipantList/controllers/handleInsertParticipantList.ts
@@ -2,29 +2,27 @@ import { Axios } from '../../base/providers/Axios'
 import { ParticipantList } from '../models/ParticipantList'
 import { insertParticipantList } from '../services/insertParticipantList'
 
-interface handleInsertParticipantListParams {
+interface HandleInsertParticipantListParams {
   item: ParticipantList
 }
 
-async function handleInsertParticipantList(params: handleInsertParticipantListParams): Promise<{error: string}> {
-  const { item } = params;
-  const insertParticipantListApi = new Axios({
-    baseURL: process.env.INSERT_PARTICIPANT_LIST_URL || "",
-  })
-  try {
-    await insertParticipantList({
-      api: insertParticipantListApi,
-      item,
-    })
-    return ({
-      error: ""
-    })
-  } catch (insertParticipantListError: any) {
-    console.log(insertParticipantListError)
-    return ({
-      error: "Erro ao inserir lista no banco"
-    })
+async function handleInsertParticipantList({
+  item,
+}: HandleInsertParticipantListParams): Promise<void> {
+  const baseURL = process.env.INSERT_PARTICIPANT_LIST_URL
+
+  if (!baseURL) {
+    throw new Error('INSERT_PARTICIPANT_LIST_URL não definida')
   }
+
+  const insertParticipantListApi = new Axios({
+    baseURL,
+  })
+
+  await insertParticipantList({
+    api: insertParticipantListApi,
+    item,
+  })
 }
 
 export { handleInsertParticipantList }
